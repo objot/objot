40ece7a93b3558707093bef4cbebd8dcf64df0d0
isdate <isdate@7e1abb0e-372b-0410-9268-ad332c1690bf>
2007-04-09 09:34:49 +0000

    refactoring

--- objot.java/demo/chat/model/ModelCreate.java
+++ objot.java/demo/chat/model/ModelCreate.java
@@ -12,10 +12,10 @@ public class ModelCreate
 	/** @param args whether to export, whether generate drop, false by default */
 	public static void main(String... args) throws Exception
 	{
-		SchemaExport se = new SchemaExport(Model.init());
+		SchemaExport sch = new SchemaExport(Model.init());
 		String name = File.createTempFile(ModelCreate.class.getName(), "").getCanonicalPath();
-		se.setOutputFile(name);
-		se.execute(false, args.length < 1 ? false : Boolean.valueOf(args[0]), //
+		sch.setOutputFile(name);
+		sch.execute(false, args.length < 1 ? false : Boolean.valueOf(args[0]), //
 			false, args.length < 2 ? true : ! Boolean.valueOf(args[1]));
 
 		File f = new File(name);
@@ -26,5 +26,8 @@ public class ModelCreate
 			if ((done = read.read(s, from, s.length - from)) < 0)
 				throw new EOFException();
 		System.out.print(s);
+
+		for (Object e: sch.getExceptions())
+			((Exception)e).printStackTrace();
 	}
 }
