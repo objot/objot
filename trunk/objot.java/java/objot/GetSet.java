a869fcf758075c4433cf9bf2f7183ec6ee055f8e
isdate <isdate@7e1abb0e-372b-0410-9268-ad332c1690bf>
2007-06-25 07:37:15 +0000

    minor improvement about @Get @Set ...

--- trunk/objot.java/java/objot/GetSet.java
+++ trunk/objot.java/java/objot/GetSet.java
@@ -4,7 +4,9 @@
 //
 package objot;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,6 +15,8 @@ import java.lang.annotation.Target;
 /** the annotated field could be get while {@link Getting#go} or {@link Setting#go} */
 @Target( { ElementType.TYPE, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface GetSet
 {
 	/**
