44f8a1e33c4099d4ef1bb0d486cc5ea17742cce3
isdate <isdate@7e1abb0e-372b-0410-9268-ad332c1690bf>
2007-08-01 13:50:25 +0000

    use Guice as service container

--- objot.java/demo/chat/service/DoUser.java
+++ objot.java/demo/chat/service/DoUser.java
@@ -14,20 +14,20 @@ public class DoUser
 {
 	/** @return me with {@link User#friends_} */
 	@Service
-	public static User me(Do $) throws Exception
+	public User me() throws Exception
 	{
-		User me = $.load(User.class, $.me);
+		User me = data.load(User.class, sess.me);
 		me.friends_ = me.friends;
 		return me;
 	}
 
 	/** update {@link User#friends} if SO' is not null */
 	@Service
-	public static Ok update(User u, Do $) throws Exception
+	public Ok update(User u) throws Exception
 	{
 		if (u.friends_ == null) // || name
 			return Ok.OK;
-		User me = $.load(User.class, $.me);
+		User me = data.load(User.class, sess.me);
 		if (u.friends_ != null)
 			me.friends = u.friends_;
 		return Ok.OK;
@@ -39,11 +39,11 @@ public class DoUser
 	 * @return array of User or null if not found
 	 */
 	@Service
-	public static User[] get(User[] us, Do $) throws Exception
+	public User[] get(User[] us) throws Exception
 	{
 		for (int i = 0; i < us.length; i++)
-			us[i] = us[i].id != null && us[i].id > 0 ? $.get(User.class, us[i].id)
-				: us[i].name != null ? $.find1(User.class, "name", us[i].name) : null;
+			us[i] = us[i].id != null && us[i].id > 0 ? data.get(User.class, us[i].id)
+				: us[i].name != null ? data.find1(User.class, "name", us[i].name) : null;
 		return us;
 	}
 }
---
+++ objot.java/demo/chat/service/common/ScopeSession.java
@@ -0,0 +1,21 @@
+package chat.service.common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.inject.ScopeAnnotation;
+
+
+/** one instance per service session. */
+@Target( { ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@ScopeAnnotation
+public @interface ScopeSession
+{
+}
