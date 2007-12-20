7dc809839496e1f0df5443db0ce91330e8387b6f
isdate <isdate@7e1abb0e-372b-0410-9268-ad332c1690bf>
2007-12-20 05:41:32 +0000

    objot flash library

--- objot-java/demo/chat/ModelsCreate.java
+++ objot-java/demo/chat/ModelsCreate.java
@@ -92,9 +92,9 @@ public class ModelsCreate
 		{
 			data.save(o.id(0));
 			data.flush();
-			String q = "update " + data.getEntityName(o) + " set id=" + id + " where id=?";
-			// no evict or update query would cause HibernateException
 			data.evict(o);
+			// after evict, or HibernateException
+			String q = "update " + data.getEntityName(o) + " set id=" + id + " where id=?";
 			if (data.query(q).setInteger(0, o.id()).executeUpdate() <= 0)
 				throw new Exception("failed persist " + o + " with id = " + id);
 			o.id(id);
