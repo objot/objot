a4811201726df4b667acc648be778d71772eae52
qianyan@cai.name <qianyan@cai.name@7e1abb0e-372b-0410-9268-ad332c1690bf>
2008-06-30 03:56:09 +0000

    license

--- trunk/objot-java/test/test/aspect/X.java
+++ trunk/objot-java/test/test/aspect/X.java
@@ -1,6 +1,6 @@
 //
 // Copyright 2007-2008 Qianyan Cai
-// Under the terms of the GNU General Public License version 2
+// Under the terms of the GNU Library General Public License version 2
 //
 package test.aspect;
 
