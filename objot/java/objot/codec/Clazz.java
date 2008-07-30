c5eff25afd6516fb7e02c9e6d58d1f72e7154a16
qianyan@cai.name <qianyan@cai.name@7e1abb0e-372b-0410-9268-ad332c1690bf>
2008-07-30 16:55:03 +0000

    config enc/dec rules by codes

--- objot/java/objot/codec/Clazz.java
+++ objot/java/objot/codec/Clazz.java
@@ -5,8 +5,7 @@
 package objot.codec;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.HashMap;
+import java.util.Map;
 
 import objot.bytecode.Bytecode;
 import objot.bytecode.Instruction;
@@ -22,53 +21,10 @@ abstract class Clazz
 {
 	static final Field F_encs = Class2.declaredField(Clazz.class, "encs");
 	Property[] encs;
-	HashMap<String, Property> decs = new HashMap<String, Property>();
+	Map<String, Property> decs;
 
-	static Clazz clazz(Class<?> c) throws Exception
-	{
-		if (c == Class.class)
-			throw new Exception(
-				"encoding/decoding class object forbidden, class literal name allowed");
-		HashMap<String, Property> es_ = new HashMap<String, Property>();
-		HashMap<String, Property> ds_ = new HashMap<String, Property>();
-		for (Field f: Class2.fields(c, 0, 0, 0))
-		{
-			Enc e = f.getAnnotation(Enc.class);
-			Dec d = f.getAnnotation(Dec.class);
-			EncDec gs = f.getAnnotation(EncDec.class);
-			if ((e != null || d != null || gs != null)
-				&& Mod2.match(f, Mod2.STATIC | Mod2.NOT_PUBLIC))
-				throw new IllegalArgumentException("encoding/decoding " + Mod2.toString(f)
-					+ f + " forbidden");
-			if (e != null || gs != null)
-				new Property(f, e, null, gs, true).into(es_);
-			if (d != null || gs != null)
-				new Property(f, null, d, gs, false).into(ds_);
-		}
-		for (Method m: Class2.methods(c, 0, 0, 0))
-		{
-			Enc e = m.getAnnotation(Enc.class);
-			Dec d = m.getAnnotation(Dec.class);
-			if ((e != null || d != null) && Mod2.match(m, Mod2.STATIC | Mod2.NOT_PUBLIC))
-				throw new IllegalArgumentException("encoding/decoding " + Mod2.toString(m)
-					+ m + " forbidden");
-			if (e != null)
-				new Property(m, e, null, true).into(es_);
-			if (d != null)
-				new Property(m, null, d, false).into(ds_);
-		}
-		Property[] es = es_.values().toArray(new Property[es_.size()]);
-		Property[] ds = ds_.values().toArray(new Property[ds_.size()]);
-		for (int i = 0; i < ds.length; i++)
-			ds[i].index = i;
-
-		Clazz z = make(c, es, ds);
-		z.encs = es;
-		z.decs = ds_;
-		return z;
-	}
-
-	private static Clazz make(Class<?> c, Property[] es, Property[] ds) throws Exception
+	static Clazz make(Class<?> c, boolean ctor, Property[] es, Property[] ds,
+		Map<String, Property> dNames) throws Exception
 	{
 		String name = Clazz.class.getName() + "$$" + c.getName().replace('.', '$');
 		Bytecode y = new Bytecode();
@@ -81,12 +37,12 @@ abstract class Clazz
 		if (es.length > 0)
 		{
 			int encsCi = y.cons.addField(F_encs);
-			int allowCi = y.cons.addProc(Property.M_allow);
+			int allowCi = y.cons.addProc(Property.M_allowEnc);
 			int nameCi = y.cons.addField(Property.F_name);
 			makeEncode(y, es, classCi, encsCi, allowCi, nameCi);
 			makeEncodeRefs(y, es, classCi, encsCi, allowCi);
 		}
-		makeObject(y, classCi);
+		makeObject(y, classCi, ctor);
 		if (ds.length > 0)
 		{
 			makeDecode(y, ds, classCi, 0);
@@ -94,7 +50,11 @@ abstract class Clazz
 			makeDecode(y, ds, classCi, 2);
 		}
 
-		return Class2.<Clazz>load(Clazz.class.getClassLoader(), name, y.normalize()).newInstance();
+		Clazz z = Class2.<Clazz>load(Clazz.class.getClassLoader(), name, y.normalize()) //
+		.newInstance();
+		z.encs = es;
+		z.decs = dNames;
+		return z;
 	}
 
 	/**
@@ -104,13 +64,13 @@ abstract class Clazz
 	 * {@link Property}[] es = {@link #encs};
 	 * A a = (A)o; // shouldn't cause ClassCastException
 	 * // no property 0
-	 * if (es[1].{@link Property#allow}(forClass) e.{@link Encoder#refs}(a.f1);
+	 * if (es[1].{@link Property#allowEnc}(ruleKey) e.{@link Encoder#refs}(a.f1);
 	 * // no property 1
-	 * if (es[3].{@link Property#allow}(forClass)) e.{@link Encoder#refs}(a.m3());
+	 * if (es[3].{@link Property#allowEnc}(ruleKey)) e.{@link Encoder#refs}(a.m3());
 	 * ...
 	 * </pre>
 	 */
-	void encodeRefs(Encoder e, Object o, Class<?> forClass)
+	void encodeRefs(Encoder e, Object o, Object ruleKey)
 	{
 	}
 
@@ -124,14 +84,14 @@ abstract class Clazz
 	 * <pre>
 	 * {@link Property}[] es = {@link #encs};
 	 * A a = (A)o; // shouldn't cause ClassCastException
-	 * if (es[0].{@link Property#allow}(forClass))
+	 * if (es[0].{@link Property#allowEnc}(ruleKey))
 	 *   e.{@link Encoder#value(String, int)}(es[0].{@link Property#name}, a.f0);
-	 * if (es[1].{@link Property#allow}(forClass))
+	 * if (es[1].{@link Property#allowEnc}(ruleKey))
 	 *   e.{@link Encoder#value(String, Object)}(es[1].{@link Property#name}, a.m1());
 	 * ...
 	 * </pre>
 	 */
-	void encode(Encoder e, Object o, Class<?> ruleKey)
+	void encode(Encoder e, Object o, Object ruleKey)
 	{
 	}
 
@@ -243,25 +203,34 @@ abstract class Clazz
 	}
 
 	/** Example: <code>return new A();</code> */
-	abstract Object object();
+	abstract Object object(Codec codec);
 
 	static final Bytes NAME_object = Bytecode.utf("object");
 	static final Bytes DESC_object = Bytecode.utf(Class2.descript(Class2.declaredMethod1(
 		Clazz.class, "object")));
 
-	private static void makeObject(Bytecode y, int classCi)
+	private static void makeObject(Bytecode y, int classCi, boolean ctor)
 	{
 		Procedure p = new Procedure(y.cons);
 		p.setModifier(Mod2.FINAL);
 		p.setNameCi(p.cons.addUtf(NAME_object));
 		p.setDescCi(p.cons.addUtf(DESC_object));
 		Instruction s = new Instruction(p.cons, 250);
-		s.insU2(NEW, classCi);
-		s.ins0(DUP);
-		s.insU2(INVOKESPECIAL, p.cons.addCtor0(classCi));
+		if (ctor)
+		{
+			s.insU2(NEW, classCi);
+			s.ins0(DUP);
+			s.insU2(INVOKESPECIAL, p.cons.addCtor0(classCi));
+		}
+		else
+		{
+			s.ins0(ALOAD1);
+			s.insU2(LDCW, classCi);
+			s.insU2(INVOKEVIRTUAL, p.cons.addProc(Codec.M_newObject));
+		}
 		s.ins0(ARETURN);
 		p.getCode().setIns(s, false);
-		p.getCode().setLocalN(1);
+		p.getCode().setLocalN(2);
 		p.getCode().setStackN(2);
 		y.getProcs().addProc(p);
 	}
